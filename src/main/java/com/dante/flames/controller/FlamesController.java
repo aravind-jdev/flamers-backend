c38b3bf70b693cfca7df673e2d4a30de9ddbe574
aravind-jdev <aravind18l15@gmail.com>
2026-02-18 20:47:53 +0530

    CrossOrigins Added

--- src/main/java/com/dante/flames/controller/FlamesController.java
+++ src/main/java/com/dante/flames/controller/FlamesController.java
@@ -1,5 +1,9 @@
 package com.dante.flames.controller;
 
+import com.dante.flames.dto.ApiResponse;
+import com.dante.flames.dto.RelationshipRequest;
+import com.dante.flames.dto.RelationshipResponse;
+import com.dante.flames.service.FlamesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,30 +11,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dante.flames.dto.ApiResponse;
-import com.dante.flames.dto.RelationshipRequest;
-import com.dante.flames.dto.RelationshipResponse;
-import com.dante.flames.service.FlamesService;
-
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://flamers.vercel.app")
 public class FlamesController {
 
     @Autowired
     private FlamesService flamesService;
 
-@PostMapping("/relationship")
-public ApiResponse<RelationshipResponse> calculate(
-        @RequestBody RelationshipRequest request) {
-
-    RelationshipResponse response = flamesService.getRelationship(
+    @PostMapping("/relationship")
+    public ApiResponse<RelationshipResponse> calculate(
+        @RequestBody RelationshipRequest request
+    ) {
+        RelationshipResponse response = flamesService.getRelationship(
             request.getName1(),
             request.getName2(),
             request.getMode()
-    );
+        );
 
-    return new ApiResponse<>(true, response);
-}
-
+        return new ApiResponse<>(true, response);
+    }
 }
